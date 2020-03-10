cb19d5b61ead86ff74bad864e77441f9eff32bf2
udshreyag <shreya.ghate@udgama.com>
2020-03-10 21:13:35 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1001715253557607',
+        clientSecret: '3d8e57cc35a39e93b87e4564ec147c4c'
+    }
 }
